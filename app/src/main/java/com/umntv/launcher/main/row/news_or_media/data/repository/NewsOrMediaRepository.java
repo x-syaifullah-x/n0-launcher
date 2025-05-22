56ee4c24492b43fdc9ac7a78180412d08de9152d
x-syaifullah-x <x.syaifullah.x@gmail.com>
2025-05-23 03:24:05 +0700

    update

--- app/src/main/java/com/umntv/launcher/main/row/news_or_media/data/repository/NewsOrMediaRepository.java
+++ app/src/main/java/com/umntv/launcher/main/row/news_or_media/data/repository/NewsOrMediaRepository.java
@@ -20,43 +20,31 @@ public class NewsOrMediaRepository {
         if (v.isEmpty()) {
             String[] title = {
                     "YOUTUBE",
-                    "YOUTUBE KIDS",
-                    "YOUTUBE TV",
+                    "SMARTTUBE",
                     "TUBI",
-//                    YOUTUBE_ENJOYABLES,
-//                    INT_NEWS,
+                    "PLUTO TV",
+                    "PLEX",
+                    "FREEVEE",
                     "TIKTOK",
-                    "FACEBOOK",
             };
             String[] description = {
                     null,
                     null,
                     null,
                     null,
-//                    null,
-//                    "",
                     null,
                     null,
-//                    "Channels Television is a leading 24-hour news channel broadcasting live from Lagos, Nigeria - West Africa. Our mission" +
-//                            " is to produce and broadcast television programs of quality and integrity that recognize and challenge, the " +
-//                            "intelligence and curiosity of our viewers.",
-//                    "Watch CNA's 24-hour live coverage of the latest headlines and top stories from Singapore, Asia and around the world, as" +
-//                            " well as documentaries and features that bring you a deeper look at Singapore and Asian issues.",
-//                    null,
+                    null,
             };
 
             String[] packageName = {
                     "com.google.android.youtube.tv",
-                    "com.google.android.youtube.tvkids",
-                    "com.google.android.youtube.tvunplugged",
+                    "com.teamsmart.videomanager.tv",
                     "com.tubitv",
-//                    null,
-//                    null,
+                    "tv.pluto.android",
+                    "com.plexapp.android",
+                    "com.imdbtv.livingroom",
                     "com.tiktok.tv",
-                    "com.facebook.katana",
-//                    null,
-//                    null,
-//                    "com.bloomberg.btva",
             };
 
             String[] youtubeId = {
@@ -64,62 +52,37 @@ public class NewsOrMediaRepository {
                     null,
                     null,
                     null,
-//                    null,
-//                    null,
                     null,
                     null,
-//                    "-c8LoR84Xjs",
-//                    "XWq5kBlakcQ",
-//                    null,
+                    null,
             };
             String[] bannerImage = {
                     ResourceHelpers.toStringUri(R.drawable.ic_apps_banner),
-                    ResourceHelpers.toStringUri(R.drawable.ic_apps_banner),
-                    ResourceHelpers.toStringUri(R.drawable.ic_apps_banner),
-                    ResourceHelpers.toStringUri(R.drawable.ic_apps_banner),
-//                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_youtube_tv),
-//                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_yotube_enjoy_ables),
-//                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_int_news),
+                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_smarttube),
+                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_tubi),
+                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_pluto_tv),
+                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_plex),
+                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_freevee),
                     ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_titok),
-                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_haystack_news_bg),
-//                    ResourceHelpers.toStringUri(R.drawable.ic_news_african_news_banner),
-//                    ResourceHelpers.toStringUri(R.drawable.ic_news_asian_news_banner),
-//                    ResourceHelpers.toStringUri(R.drawable.ic_news_bloomberg),
             };
             String[] cardImage = {
                     ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_youtube),
-                    ResourceHelpers.toStringUri(R.drawable.kids_ic_youtube_kids),
-                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_youtube_tv),
+                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_smarttube),
                     ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_tubi),
-//                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_yotube_enjoy_ables),
-//                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_int_news),
+                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_pluto_tv),
+                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_plex),
+                    ResourceHelpers.toStringUri(R.drawable.news_and_media_bg_freevee),
                     ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_titok),
-                    ResourceHelpers.toStringUri(R.drawable.news_or_media_ic_haystack_news),
-//                    ResourceHelpers.toStringUri(R.drawable.ic_news_african_news),
-//                    ResourceHelpers.toStringUri(R.drawable.ic_news_asian_news),
-//                    ResourceHelpers.toStringUri(R.drawable.ic_news_bloomberg),
             };
-//            String[] iconDetailImage = {
-//                    null,
-//                    null,
-//                    null,
-//                    ResourceHelpers.toStringUri(R.drawable.bg_int_news),
-//                    null,
-//                    null,
-////                    ResourceHelpers.toStringUri(R.drawable.ic_news_african_news_detail_icon),
-////                    ResourceHelpers.toStringUri(R.drawable.ic_news_asian_news_detail_icon),
-////                    null,
-//            };
 
             String[] apkUrl = {
                     null,
+                    "https://n0render.com/N0Launcher/com.teamsmart.videomanager.tv_27.53-19.apk",
                     null,
                     null,
                     null,
-//                    null,
-//                    null,
                     null,
-                    "https://umntvdealers.net/UMNTV/Apks/com.facebook.katana_.apk",
+                    null,
             };
 
             for (int index = 0; index < title.length; ++index) {
